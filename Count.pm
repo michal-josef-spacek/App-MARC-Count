e950213095ee2c771591018d38c14e871e2d6448
Michal Josef Špaček <michal.josef.spacek@gmail.com>
2022-05-17 11:13:01 +0200

    Prepare for 0.03 version

--- Count.pm
+++ Count.pm
@@ -9,7 +9,7 @@ use Getopt::Std;
 use MARC::File::XML (BinaryEncoding => 'utf8', RecordFormat => 'MARC21');
 use Unicode::UTF8 qw(encode_utf8);
 
-our $VERSION = 0.02;
+our $VERSION = 0.03;
 
 # Constructor.
 sub new {
@@ -300,6 +300,6 @@ BSD 2-Clause License
 
 =head1 VERSION
 
-0.02
+0.03
 
 =cut
